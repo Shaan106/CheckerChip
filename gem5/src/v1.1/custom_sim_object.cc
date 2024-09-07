50509acbaeec32d7ee60d12d2262c6dfa2e4f04b
Shaan <shaan@yadu.in>
2024-09-07 15:23:54 -0400

    simobj w custom event handling

--- gem5/src/v1.1/custom_sim_object.cc
+++ gem5/src/v1.1/custom_sim_object.cc
@@ -1,4 +1,6 @@
 #include "v1.1/custom_sim_object.hh"
+#include "base/trace.hh"
+#include "debug/CustomSimObjectFlag.hh"
 
 #include <iostream>
 
@@ -6,9 +8,38 @@ namespace gem5
 {
 
 CustomSimObject::CustomSimObject(const CustomSimObjectParams &params) :
-    SimObject(params)
+    // SimObject(params) - this is the base class constructor
+    // event([this]{processEvent();}, name()) - [this] is a lambda capture list, {processEvent();} is the lambda body, name() is the name of the SimObject
+    SimObject(params), 
+    event([this]{processEvent();}, name()),
+    latency(100), // custom defined parameter, passing in a value here
+    timesLeft(10)
 {
-    std::cout << "Hello World! From a CustomSimObject!" << std::endl;
+    DPRINTF(CustomSimObjectFlag, "Created the customSimObject\n");
+}
+
+// for the event to be processed, we first have to schedule the event. 
+// The startup() function is where SimObjects are allowed to schedule internal events.
+// does not get executed until the simulation begins for the first time (i.e. the simulate() function is called from a Python config file).
+void
+CustomSimObject::startup()
+{
+    // we simply schedule the event to execute at tick 100. Normally, you would use some offset from curTick(), but since we know the startup() function is called when the time is currently 0, we can use an explicit tick value.
+    schedule(event, 100);
+
+}
+
+void
+CustomSimObject::processEvent()
+{
+    timesLeft--;
+    DPRINTF(CustomSimObjectFlag, "Firing event from within custom sim object. Event counter: %d\n", timesLeft);
+
+    if (timesLeft <= 0) {
+        DPRINTF(CustomSimObjectFlag, "Done firing!\n");
+    } else {
+        schedule(event, curTick() + latency);
+    }
 }
 
 } // namespace gem5
--- notes.md
+++ notes.md
@@ -8,10 +8,18 @@ All tutorial work is within /playground/
 
 `scons build/X86/gem5.opt -j<cores>`
 
-## make buff notes
+## make buffer (simobject) notes
+
+### part 1
+[gem5 learn docs](https://www.gem5.org/documentation/learning_gem5/part2/debugging/)
 
 `./build/X86/gem5.opt --debug-help` provides all of the debug flags. These would be useful for printing debug statements for example when changing commit source code.
 
 ie for commit: `./build/X86/gem5.opt --debug-flags=Commit _checker/v0.1/hello-world.py`
+only top few statements: `./build/X86/gem5.opt --debug-flags=Commit _checker/v0.1/hello-world.py | head -n 50`
 
+### part 2 - custom debug flags
 
+use new custom debug flag with `./build/X86/gem5.opt --debug-flags=CustomSimObjectFlag `
+## other
+[gem5 cpu docs](https://www.gem5.org/documentation/general_docs/cpu_models/O3CPU##Pipeline-stages)
