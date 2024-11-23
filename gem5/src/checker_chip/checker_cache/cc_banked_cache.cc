3984e2735c1ee0aedd813a042d9056ac56d25231
Shaan106 <shaan@yadu.in>
2024-11-23 16:29:07 -0500

    v10.0.3 Banked Cache Specific model

--- gem5/src/checker_chip/checker_cache/cc_banked_cache.cc
+++ gem5/src/checker_chip/checker_cache/cc_banked_cache.cc
@@ -54,26 +54,13 @@ CC_BankedCache::calculateBankId(Addr addr)
         [this, bankID]() { freeBank(bankID); }, name() + ".freeBank", true), 
         curTick() + 20 * clockPeriod());
 
-    DPRINTF(CC_BankedCache, "bankFreeList: \n");
-    for (size_t i = 0; i < bankFreeList.size(); ++i) {
-        DPRINTF(CC_BankedCache, "[%d] = %s ", i, bankFreeList[i] ? "true" : "false");
-    }
-    DPRINTF(CC_BankedCache, "\n");
+    // DPRINTF(CC_BankedCache, "bankFreeList: \n");
+    // for (size_t i = 0; i < bankFreeList.size(); ++i) {
+    //     DPRINTF(CC_BankedCache, "[%d] = %s \n", i, bankFreeList[i] ? "true" : "false");
+    // }
+    // DPRINTF(CC_BankedCache, "\n");
 
     return bankID;
-}
-
-bool
-CC_BankedCache::access(PacketPtr pkt, CacheBlk *&blk, Cycles &lat,
-                       PacketList &writebacks)
-{
-    // Determine the bank ID based on the address
-    unsigned bankId = calculateBankId(pkt->getAddr());
-
-    // Print bank ID
-    DPRINTF(CC_BankedCache, "Accessing bank ID: %u for address: %#x\n", bankId, pkt->getAddr());
-
-    return Cache::access(pkt, blk, lat, writebacks);
 }
 
 Port &
@@ -222,8 +209,7 @@ CC_BankedCache::recvTimingReq(PacketPtr pkt)
         // a cache above us (but not where the packet came from) is
         // responding to the request, in other words it has the line
         // in Modified or Owned state
-        DPRINTF(Cache, "Cache above responding to %s: not responding\n",
-                pkt->print());
+        // DPRINTF(Cache, "Cache above responding to %s: not responding\n", pkt->print());
 
         // if the packet needs the block to be writable, and the cache
         // that has promised to respond (setting the cache responding
@@ -330,8 +316,7 @@ CC_BankedCache::recvTimingReq(PacketPtr pkt)
         ppHit->notify(CacheAccessProbeArg(pkt,accessor));
 
         if (prefetcher && blk && blk->wasPrefetched()) {
-            DPRINTF(Cache, "Hit on prefetch for addr %#x (%s)\n",
-                    pkt->getAddr(), pkt->isSecure() ? "s" : "ns");
+            // DPRINTF(Cache, "Hit on prefetch for addr %#x (%s)\n", pkt->getAddr(), pkt->isSecure() ? "s" : "ns");
             blk->clearPrefetched();
         }
 
@@ -352,5 +337,300 @@ CC_BankedCache::recvTimingReq(PacketPtr pkt)
     }
     
 }
+
+bool
+CC_BankedCache::access(PacketPtr pkt, CacheBlk *&blk, Cycles &lat,
+                       PacketList &writebacks)
+{
+    // Determine the bank ID based on the address
+    unsigned bankId = calculateBankId(pkt->getAddr());
+
+    // Print bank ID
+    DPRINTF(CC_BankedCache, "Accessing bank ID: %u for address: %#x\n", bankId, pkt->getAddr());
+
+    if (pkt->req->isUncacheable()) {
+        assert(pkt->isRequest());
+
+        gem5_assert(!(isReadOnly && pkt->isWrite()),
+                    "Should never see a write in a read-only cache %s\n",
+                    name());
+
+        DPRINTF(CC_BankedCache, "%s for %s\n", __func__, pkt->print());
+
+        // flush and invalidate any existing block
+        CacheBlk *old_blk(tags->findBlock(pkt->getAddr(), pkt->isSecure()));
+        if (old_blk && old_blk->isValid()) {
+            BaseCache::evictBlock(old_blk, writebacks);
+        }
+
+        blk = nullptr;
+        // lookupLatency is the latency in case the request is uncacheable.
+        lat = lookupLatency;
+        return false;
+    }
+
+    // sanity check
+    assert(pkt->isRequest());
+
+    gem5_assert(!(isReadOnly && pkt->isWrite()),
+                "Should never see a write in a read-only cache %s\n",
+                name());
+
+    // Access block in the tags
+    Cycles tag_latency(0);
+    blk = tags->accessBlock(pkt, tag_latency);
+
+    DPRINTF(CC_BankedCache, "%s for %s %s\n", __func__, pkt->print(), blk ? "hit " + blk->print() : "miss");
+
+    if (pkt->req->isCacheMaintenance()) {
+        // A cache maintenance operation is always forwarded to the
+        // memory below even if the block is found in dirty state.
+
+        // We defer any changes to the state of the block until we
+        // create and mark as in service the mshr for the downstream
+        // packet.
+
+        // Calculate access latency on top of when the packet arrives. This
+        // takes into account the bus delay.
+        lat = calculateTagOnlyLatency(pkt->headerDelay, tag_latency);
+
+        return false;
+    }
+
+    if (pkt->isEviction()) {
+        // We check for presence of block in above caches before issuing
+        // Writeback or CleanEvict to write buffer. Therefore the only
+        // possible cases can be of a CleanEvict packet coming from above
+        // encountering a Writeback generated in this cache peer cache and
+        // waiting in the write buffer. Cases of upper level peer caches
+        // generating CleanEvict and Writeback or simply CleanEvict and
+        // CleanEvict almost simultaneously will be caught by snoops sent out
+        // by crossbar.
+        WriteQueueEntry *wb_entry = writeBuffer.findMatch(pkt->getAddr(),
+                                                          pkt->isSecure());
+        if (wb_entry) {
+            assert(wb_entry->getNumTargets() == 1);
+            PacketPtr wbPkt = wb_entry->getTarget()->pkt;
+            assert(wbPkt->isWriteback());
+
+            if (pkt->isCleanEviction()) {
+                // The CleanEvict and WritebackClean snoops into other
+                // peer caches of the same level while traversing the
+                // crossbar. If a copy of the block is found, the
+                // packet is deleted in the crossbar. Hence, none of
+                // the other upper level caches connected to this
+                // cache have the block, so we can clear the
+                // BLOCK_CACHED flag in the Writeback if set and
+                // discard the CleanEvict by returning true.
+                wbPkt->clearBlockCached();
+
+                // A clean evict does not need to access the data array
+                lat = calculateTagOnlyLatency(pkt->headerDelay, tag_latency);
+
+                return true;
+            } else {
+                assert(pkt->cmd == MemCmd::WritebackDirty);
+                // Dirty writeback from above trumps our clean
+                // writeback... discard here
+                // Note: markInService will remove entry from writeback buffer.
+                markInService(wb_entry);
+                delete wbPkt;
+            }
+        }
+    }
+
+    // The critical latency part of a write depends only on the tag access
+    if (pkt->isWrite()) {
+        lat = calculateTagOnlyLatency(pkt->headerDelay, tag_latency);
+    }
+
+    // Writeback handling is special case.  We can write the block into
+    // the cache without having a writeable copy (or any copy at all).
+    if (pkt->isWriteback()) {
+        assert(blkSize == pkt->getSize());
+
+        // we could get a clean writeback while we are having
+        // outstanding accesses to a block, do the simple thing for
+        // now and drop the clean writeback so that we do not upset
+        // any ordering/decisions about ownership already taken
+        if (pkt->cmd == MemCmd::WritebackClean &&
+            mshrQueue.findMatch(pkt->getAddr(), pkt->isSecure())) {
+            // DPRINTF(Cache, "Clean writeback %#llx to block with MSHR, dropping\n", pkt->getAddr());
+
+            // A writeback searches for the block, then writes the data.
+            // As the writeback is being dropped, the data is not touched,
+            // and we just had to wait for the time to find a match in the
+            // MSHR. As of now assume a mshr queue search takes as long as
+            // a tag lookup for simplicity.
+            return true;
+        }
+
+        const bool has_old_data = blk && blk->isValid();
+        if (!blk) {
+            // need to do a replacement
+            blk = allocateBlock(pkt, writebacks);
+            if (!blk) {
+                // no replaceable block available: give up, fwd to next level.
+                incMissCount(pkt);
+                return false;
+            }
+
+            blk->setCoherenceBits(CacheBlk::ReadableBit);
+        } else if (compressor) {
+            // This is an overwrite to an existing block, therefore we need
+            // to check for data expansion (i.e., block was compressed with
+            // a smaller size, and now it doesn't fit the entry anymore).
+            // If that is the case we might need to evict blocks.
+            if (!updateCompressionData(blk, pkt->getConstPtr<uint64_t>(),
+                writebacks)) {
+                invalidateBlock(blk);
+                return false;
+            }
+        }
+
+        // only mark the block dirty if we got a writeback command,
+        // and leave it as is for a clean writeback
+        if (pkt->cmd == MemCmd::WritebackDirty) {
+            // TODO: the coherent cache can assert that the dirty bit is set
+            blk->setCoherenceBits(CacheBlk::DirtyBit);
+        }
+        // if the packet does not have sharers, it is passing
+        // writable, and we got the writeback in Modified or Exclusive
+        // state, if not we are in the Owned or Shared state
+        if (!pkt->hasSharers()) {
+            blk->setCoherenceBits(CacheBlk::WritableBit);
+        }
+        // nothing else to do; writeback doesn't expect response
+        assert(!pkt->needsResponse());
+
+        updateBlockData(blk, pkt, has_old_data);
+        // DPRINTF(Cache, "%s new state is %s\n", __func__, blk->print());
+        incHitCount(pkt);
+
+        // When the packet metadata arrives, the tag lookup will be done while
+        // the payload is arriving. Then the block will be ready to access as
+        // soon as the fill is done
+        blk->setWhenReady(clockEdge(fillLatency) + pkt->headerDelay +
+            std::max(cyclesToTicks(tag_latency), (uint64_t)pkt->payloadDelay));
+
+        return true;
+    } else if (pkt->cmd == MemCmd::CleanEvict) {
+        // A CleanEvict does not need to access the data array
+        lat = calculateTagOnlyLatency(pkt->headerDelay, tag_latency);
+
+        if (blk) {
+            // Found the block in the tags, need to stop CleanEvict from
+            // propagating further down the hierarchy. Returning true will
+            // treat the CleanEvict like a satisfied write request and delete
+            // it.
+            return true;
+        }
+        // We didn't find the block here, propagate the CleanEvict further
+        // down the memory hierarchy. Returning false will treat the CleanEvict
+        // like a Writeback which could not find a replaceable block so has to
+        // go to next level.
+        return false;
+    } else if (pkt->cmd == MemCmd::WriteClean) {
+        // WriteClean handling is a special case. We can allocate a
+        // block directly if it doesn't exist and we can update the
+        // block immediately. The WriteClean transfers the ownership
+        // of the block as well.
+        assert(blkSize == pkt->getSize());
+
+        const bool has_old_data = blk && blk->isValid();
+        if (!blk) {
+            if (pkt->writeThrough()) {
+                // if this is a write through packet, we don't try to
+                // allocate if the block is not present
+                return false;
+            } else {
+                // a writeback that misses needs to allocate a new block
+                blk = allocateBlock(pkt, writebacks);
+                if (!blk) {
+                    // no replaceable block available: give up, fwd to
+                    // next level.
+                    incMissCount(pkt);
+                    return false;
+                }
+
+                blk->setCoherenceBits(CacheBlk::ReadableBit);
+            }
+        } else if (compressor) {
+            // This is an overwrite to an existing block, therefore we need
+            // to check for data expansion (i.e., block was compressed with
+            // a smaller size, and now it doesn't fit the entry anymore).
+            // If that is the case we might need to evict blocks.
+            if (!updateCompressionData(blk, pkt->getConstPtr<uint64_t>(),
+                writebacks)) {
+                invalidateBlock(blk);
+                return false;
+            }
+        }
+
+        // at this point either this is a writeback or a write-through
+        // write clean operation and the block is already in this
+        // cache, we need to update the data and the block flags
+        assert(blk);
+        // TODO: the coherent cache can assert that the dirty bit is set
+        if (!pkt->writeThrough()) {
+            blk->setCoherenceBits(CacheBlk::DirtyBit);
+        }
+        // nothing else to do; writeback doesn't expect response
+        assert(!pkt->needsResponse());
+
+        updateBlockData(blk, pkt, has_old_data);
+        // DPRINTF(Cache, "%s new state is %s\n", __func__, blk->print());
+
+        incHitCount(pkt);
+
+        // When the packet metadata arrives, the tag lookup will be done while
+        // the payload is arriving. Then the block will be ready to access as
+        // soon as the fill is done
+        blk->setWhenReady(clockEdge(fillLatency) + pkt->headerDelay +
+            std::max(cyclesToTicks(tag_latency), (uint64_t)pkt->payloadDelay));
+
+        // If this a write-through packet it will be sent to cache below
+        return !pkt->writeThrough();
+    } else if (blk && (pkt->needsWritable() ?
+            blk->isSet(CacheBlk::WritableBit) :
+            blk->isSet(CacheBlk::ReadableBit))) {
+        // OK to satisfy access
+        incHitCount(pkt);
+
+        // Calculate access latency based on the need to access the data array
+        if (pkt->isRead()) {
+            lat = calculateAccessLatency(blk, pkt->headerDelay, tag_latency);
+
+            // When a block is compressed, it must first be decompressed
+            // before being read. This adds to the access latency.
+            if (compressor) {
+                lat += compressor->getDecompressionLatency(blk);
+            }
+        } else {
+            lat = calculateTagOnlyLatency(pkt->headerDelay, tag_latency);
+        }
+
+        satisfyRequest(pkt, blk);
+        maintainClusivity(pkt->fromCache(), blk);
+
+        return true;
+    }
+
+    // Can't satisfy access normally... either no block (blk == nullptr)
+    // or have block but need writable
+
+    incMissCount(pkt);
+
+    lat = calculateAccessLatency(blk, pkt->headerDelay, tag_latency);
+
+    if (!blk && pkt->isLLSC() && pkt->isWrite()) {
+        // complete miss on store conditional... just give up now
+        pkt->req->setExtraData(0);
+        return true;
+    }
+
+    return false;
+}
+
 
 } // namespace gem5
